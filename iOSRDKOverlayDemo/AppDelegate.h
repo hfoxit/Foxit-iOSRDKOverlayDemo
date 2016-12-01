848f119321d3fc0780af90e3202dea480caaf040
hong <hong_zhang@foxitsoftware.com>
2016-12-01 11:49:19 -0800

    Initial Commit

---
+++ iOSRDKOverlayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSRDKOverlayDemo
+//
+//  Created by foxit on 12/1/16.
+//  Copyright © 2016 Foxit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
